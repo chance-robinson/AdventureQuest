94d05dcf04330be405b414bae01a450aae52edb4
Chance Robinson <brighamyoung2@gmail.com>
2023-05-26 06:34:37 -0600

    dummy vals

--- destinyQuest/src/assets/carouselData.jsx
+++ destinyQuest/src/assets/carouselData.jsx
@@ -1,26 +1,26 @@
 export const carouselData = [
     {
         image: 'src/assets/carouselImages/download.jpg',
-        title: 'randomTitle0',
-        text: 'randomText0',
+        title: 'Title0',
+        text: 'Text0',
         link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     },
     {
         image: 'src/assets/carouselImages/download (1).jpg',
-        title: 'randomTitle1',
-        text: 'randomText1',
-        link: 'randomLink1',
+        title: 'Title1',
+        text: 'Text1',
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     },
     {
         image: 'src/assets/carouselImages/download (2).jpg',
-        title: 'randomTitle2',
-        text: 'randomText2',
-        link: 'randomLink2',
+        title: 'Title2',
+        text: 'Text2',
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     },
     {
         image: 'src/assets/carouselImages/download (3).jpg',
-        title: 'randomTitle3',
-        text: 'randomText3',
-        link: 'randomLink3',
+        title: 'Title3',
+        text: 'Text3',
+        link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     },
 ]
